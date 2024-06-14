bfb25158390189584598cb1d8add2bd479d8989f
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-14 09:39:55 +0500

    class-ract-redux

---
+++ CLASS-REACT/class-react-redux/src/features/counter/counterSlice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+/**
+ * createSlice the help todo state update, change, action
+ * (point) createSlice => (object {}), (property), (function)
+ */
+export const counterSlice = createSlice({})
