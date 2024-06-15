8254c8062d31f77f8d35fe211b3eaa0482683489
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-15 10:20:39 +0500

    commint out

--- CLASS-REACT/class-react-redux/src/Components/store.js
+++ CLASS-REACT/class-react-redux/src/Components/store.js
@@ -4,6 +4,6 @@ import counterSlice from "../features/counter/counterSlice"
 export const store = configureStore({
     // id: 1,
     reducer: {
-        counter: counterSlice
+        counter: counterSlice   //  counter is state
     }
 })
