06006e66279b2a3a675b1012e6601b2e9d0756ac
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-15 11:30:15 +0500

    set the amount and add button

--- CLASS-REACT/class-react-redux/src/features/counter/Counter.jsx
+++ CLASS-REACT/class-react-redux/src/features/counter/Counter.jsx
@@ -3,29 +3,50 @@
  * (point)  2: useDispatch => (Data Update) karwane kylye he
  */
 
-// import React from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./counterSlice";
+import { decrement, increment, incrementByAmount } from "./counterSlice";
 
 export default function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-
+  // console.log(count);
+  const [inputField, setInputField] = useState();
+  console.log(inputField);
   return (
     <div>
       <div>
+        {/* INCREMENT */}
         <button
           aria-label="Increment value"
-          onClick={() => dispatch(increment())}
+          onClick={() =>
+            count === 100 ? alert("Limited value 100") : dispatch(increment())
+          }
         >
           Increment
         </button>
         <span>{count}</span>
+        {/* DECREMENT */}
         <button
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           Decrement
+        </button>
+        {/* INCREMENT AMOUNT */}
+        <input
+          type="number"
+          aria-label="amount"
+          value={inputField}
+          onChange={(e) => setInputField(e.target.value)}
+          onBlur={(e) => setInputField(e.target.value)}
+        />
+
+        <button
+          aria-label="Increment Amount Value"
+          onClick={() => dispatch(incrementByAmount(inputField))}
+        >
+          Increment Amount
         </button>
       </div>
     </div>
